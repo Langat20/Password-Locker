1325fad2eaa1fbe606fe906252c80dc844acd876
Langat20 <kipkuruie7.lang@gmail.com>
2022-04-25 20:09:37 +0300

    add testcase to test if the credential object is saved into the credential list

--- credentials_test.py
+++ credentials_test.py
@@ -20,7 +20,15 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credential.credential_name, "twitter")
         self.assertEqual(self.new_credential.credential_password, "Uy67tye")
         
-  
+    def test_save_credentials(self):
+        '''
+        Test case to test if the credential object is saved into the credential list
+        '''
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list), 1)
+        
+        
+   
         
 if __name__ == "__main__":
     unittest.main()
