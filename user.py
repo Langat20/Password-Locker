2d9a8123d428f7c898f9b9517d19b8ac40ada9ab
Langat20 <kipkuruie7.lang@gmail.com>
2022-04-25 20:51:07 +0300

    add a method to allow a user to login into an account

--- user.py
+++ user.py
@@ -16,4 +16,29 @@ class User:
         self.username = username
         self.password = password
         
+    def save_user(self):
+        '''
+        a method to save a new user instance into the user_list 
+        '''
+        User.user_list.append(self)
+        
+    @classmethod
+    def display_user(cls):
+        return cls.user_list 
+    
+    @classmethod
+    def log_in(cls, name, passcode):
+        '''
+        method to allow a user to login into their account
+        
+        returns: 
+        user credentials if the name and passcode are correct for the user 
+        False: for incorrect name or passcode
+        '''
+        for user in cls.user_list:
+            if user.username==name and user.password==passcode:
+                return Credential.credential_list
+        return False
+    
+    
         
