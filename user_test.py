924977c037e88375aec6a1bcb8ac4bd04b9f33ce
Langat20 <kipkuruie7.lang@gmail.com>
2022-04-25 21:14:04 +0300

    test to test whether the user is being added to the user list

--- user_test.py
+++ user_test.py
@@ -25,4 +25,11 @@ class test_user(unittest.TestCase):
         self.assertEqual(self.new_user.username, "Evans")
         self.assertEqual(self.new_user.password, "langat")
         
-   
+    def test_save_user(self):
+        '''
+        To test if a user is being added to the user user_list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+    
+    
